6a0d8f9d185994b36cbd8aaaf8357104fb6a4d0e
JKurland <joshua.kurland@outlook.com>
2017-06-28 21:13:41 +0100

    todo

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 from simple_layers import Linear, Constant, Relu
 
 c = Constant('input', np.random.rand(10,20))
+c2 = Constant('input2', np.random.rand(10,33))
 
 lin = Linear('linear', 23)
 
@@ -12,6 +13,7 @@ relu = Relu('relu')
 
 relu.add_input(lin)
 lin.add_input(c)
+lin.add_input(c2)
 
 relu.make_real()
 
