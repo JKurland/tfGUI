e963c6e57db9fae85d69c8035e2a34c8606ec07d
JKurland <joshua.kurland@outlook.com>
2017-07-03 13:19:08 +0100

    changed how Constant works, added graph reset when teh old graph cannot be reused

--- simple_layers.py
+++ simple_layers.py
@@ -1,5 +1,5 @@
 from layer_base import *
-
+import numpy as np
 
 
 
@@ -24,12 +24,14 @@ class Linear(LayerBase):
     """
 
     def __init__(self, name, canvas, size=20):
-        LayerBase.__init__(self, name, canvas, [None,None], allow_multiple_inputs =
-                           True)
+        LayerBase.__init__(self, name, canvas, input_shapes = [[None, -1]])
+        #None forces all inputs to be the same size, -1 allows inputs to be
+        #different
         self._size = size
 
     
     def proc(self, inputs):
+        inputs = inputs['main']
         in_size = inputs.get_shape().as_list()[-1]
 
         w = use_variable(self.name, 'weights', [in_size, self._size])
@@ -47,27 +49,56 @@ class Relu(LayerBase):
     """
 
     def __init__(self, name, canvas):
-        LayerBase.__init__(self, name, canvas, None, allow_multiple_inputs = True)
+        LayerBase.__init__(self, name, canvas)
 
 
     def proc(self, inputs):
-
+        inputs = inputs['main']
         with tf.variable_scope(self.name):
             return tf.nn.relu(inputs, 'relu')
 
+
 class Constant(LayerBase):
     """layer which outputs a constant numpy array"""
-
-    def __init__(self, name, canvas, value=0):
-        LayerBase.__init__(self, name, canvas, [-1]) #required shape of [-1] means no
-        #input can be accepted
-        self.real = True
-        self.output = tf.convert_to_tensor(value, tf.float32)
     
-    def reset_real(self):
-        self.real = True
 
-#because self.real is set to true proc and pre_proc will never be called
+    def __init__(self, name, canvas, value=np.random.rand(5,10)):
+        LayerBase.__init__(self, name, canvas, input_number = 0)
+        self.value = value
+   
+    def proc(self, inputs):
+        return tf.convert_to_tensor(self.value, tf.float32)
+
+class Print(LayerBase):
+    """layer which prints its input when run"""
+
+    def __init__(self, name, canvas):
+        LayerBase.__init__(self, name, canvas)
+
+    def run(self,session):
+        print(session.run(self.output))
+
+
+
+
+def add_layer(Class, man):
+    name = Class.__name__
+    i = 0
+    new_name = '%s%i'%(name, i)
+    if name in list(man.names):
+        while new_name in list(man.names):
+            i+=1
+            new_name = '%s%i'%(name, i)
+
+        name = new_name
+        
+        
+    new_layer = Class(name, man.canvas)
+    man.id2obj[new_layer.id] = new_layer
+    man.obj2id[new_layer] = new_layer.id
+    man.names.append(new_layer.name)
+    man.layers.append(new_layer)
+
 
 
 
@@ -77,30 +108,35 @@ class DragManager():
     """
 
     def __init__(self, canvas):
-        self.widget = None
+        self.widget = None 
+        self.sess = tf.Session()
         self.canvas = canvas
         self.mouse_x = None
         self.mouse_y = None
         self.id2obj = {}
         self.obj2id = {}
         self.names = []
+        self.layers = []
+        self.logs = {}
         self.shortcut_dict = {'l':Linear,
                               'c':Constant,
-                              'r':Relu}
-
+                              'r':Relu,
+                              'p':Print}
+        self.tags = None 
 
     def on_click(self, event):
         self.widget = self.canvas.find_closest(event.x, event.y, halo = 5)[0]
         self.mouse_x = event.x
         self.mouse_y = event.y
 
+
     def on_move(self, event):
         if self.widget:
             ID = self.canvas.gettags(self.widget)
-            if len(ID) >1 : #an arrow got selected
+            if 'arrow' in ID: #an arrow got selected
                 return
 
-            widgets = self.canvas.find_withtag(ID)
+            widgets = self.canvas.find_withtag(ID[0])
             for w in widgets:
                 self.canvas.move(w, event.x - self.mouse_x,
                                  event.y - self.mouse_y)
@@ -132,47 +168,86 @@ class DragManager():
                 y0 += event.y - self.mouse_y
 
                 self.canvas.coords(a, x0, y0, x1, y1)
- 
+             
             
             self.mouse_x = event.x
             self.mouse_y = event.y
     
     def on_release(self, event):
-        self.widget = None
+        self.widget =  None
         self.mouse_x = None
         self.mouse_y = None
 
+    def on_double(self, event):
+        #self.widget is set by the on_click event which is called first
+        widget = self.canvas.find_closest(event.x, event.y, halo = 5)[0]
+
+        tags = self.canvas.gettags(widget)
+        
+        if 'arrow' not in tags:
+            obj = self.id2obj[tags[0]]
+        else:
+            return
+        
+        if not obj.real:
+            self.sess.close()
+            tf.reset_default_graph()
+            self.sess = tf.Session()
+            #as the graph is being cleared we can't keep anything from
+            #previous realifying so reset. Reseting the obj will reset
+            #everything in the same connected region as the obj
+            obj.reset_real()
+            if obj.make_real() == 0:
+                print("realifying failed")
+                return 
+            init = tf.global_variables_initializer()
+            self.sess.run(init)
+
+        for l in self.layers:
+            if l.real:
+                l.run(self.sess)
+
+
     def on_r_click(self, event):
-        self.widget = self.canvas.find_closest(event.x, event.y, halo = 5)[0]
-        self.widget = self.canvas.gettags(self.widget)[0] 
+        self.widget = self.canvas.find_closest(event.x, event.y, halo = 5)[0] 
+        self.tags = self.canvas.gettags(self.widget)
 
     def on_r_release(self, event):
         other_widget = self.canvas.find_closest(event.x, event.y, halo = 5)[0]
-        other_widget = self.id2obj[self.canvas.gettags(other_widget)[0]]
-        other_widget.add_input(self.id2obj[self.widget])
+        other_tags = self.canvas.gettags(other_widget) 
+        print(other_tags)
+        if 'arrow' in other_tags or 'arrow' in self.tags:
+            return #if either object is an arrow return
+        
+        if 'input' not in other_tags:
+            return #the target is not an input so return
 
+        obj = self.id2obj[self.canvas.gettags(self.widget)[0]]
+        other_obj = self.id2obj[self.canvas.gettags(other_widget)[0]]
+        
+        socket = other_tags[2]
+        print(obj)
+        print(other_obj)
+        
+        for k in obj._inputs.keys():
+            if other_obj in obj._inputs[k]:
+                obj.remove_input(other_obj,k)
+                return 
+
+        if obj in other_obj._inputs[socket]:
+            other_obj.remove_input(obj,socket)
+
+        else:
+            other_obj.add_input(obj,socket)
+        
     def on_key(self, event):
         print(event.char)
         try:
             Class = self.shortcut_dict[event.char]
         except:
             return
-        name = Class.__name__
-        i = 0
-        new_name = '%s%i'%(name, i)
-        if name in list(self.names):
-            while new_name in list(self.names):
-                i+=1
-                new_name = '%s%i'%(name, i)
-
-            name = new_name
-        
-        
-        new_layer = Class(name, self.canvas)
-        self.id2obj[new_layer.id] = new_layer
-        self.obj2id[new_layer] = new_layer.id
-        self.names.append(new_layer.name)
-    
+        add_layer(Class, self)
+      
 
         
 
